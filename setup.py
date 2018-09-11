5c77011fe4da2db72f479cf23c22caea1504db65
opteroncx <opteroncx@gmail.com>
2018-09-11 09:59:50 +0800

    clean files

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ os.environ['TCL_LIBRARY'] = 'C:\\Users\\opteroncx.000\\Py36\\tcl\\tcl8.6'
 os.environ['TK_LIBRARY'] = 'C:\\Users\\opteroncx.000\\Py36\\tcl\\tk8.6'
 # Dependencies are automatically detected, but it might need fine tuning.
 
+# include files
 ifiles = ['./models.pyc',
-        './model6dn.pyc',
-        './dn.pyc',
         './dehaze.pyc',
         './model.pyc',
         './runDN.pyc',
@@ -25,10 +24,23 @@ ifiles = ['./models.pyc',
         './nvidia-smi.exe'
         ]
 
+# exclude files
+efiles = ['./model/__pycache__',
+        './model/old',
+        './model/convert_cpkt.py',
+        './model/dn.py',
+        './model/model.py',
+        './model/model6.py',
+        './model/model63.py',
+        './model/model64.py',
+        './model/model6dn.py'
+        ]
+
 build_exe_options = {
         'packages': ['tkinter', 'scipy', 'asyncio','numpy','torch'], 
         'includes': ['numpy.core._methods','jinja2','jinja2.ext','asyncio.compat'],
-        'include_files': ifiles}
+        'include_files': ifiles,
+        'bin_excludes': efiles}
 
 base = None
 
@@ -40,7 +52,7 @@ exe = Executable(script='MoePhoto.py', base = base, icon='logo3.ico')
 
 
 setup(  name = 'MoePhoto',
-        version = '1.5',
+        version = '2.5',
         description = 'May-workshop',
         options = {'build_exe': build_exe_options},
         executables = [exe])
