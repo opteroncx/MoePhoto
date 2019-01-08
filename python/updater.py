d6388387d9a98c660fbf6d402ce7e321223e65d3
opteroncx <opteroncx@gmail.com>
2019-01-08 15:27:17 +0800

    Update updater.py
    updater未测试

--- python/updater.py
+++ python/updater.py
@@ -3,9 +3,10 @@
 import os
 import zipfile
 import requests
-
+import codecs
 releases = 'http://www.may-workshop.com/moephoto/release.txt'
 ufile = 'http://www.may-workshop.com/moephoto/files/'
+
 
 def getVersion():
     f = open()
@@ -13,10 +14,26 @@ def getVersion():
     return fv
 
 def update():
-    v = getVersion()
-    new = ufile+v
-    # download zip
+    # make temp dir
     if not os.path.exists('./update_tmp'):
         os.mkdir('./update_tmp')
-
-    # extract zip
+    v = getVersion()
+    log_file = codecs.open('./update_log.txt','utf-8')
+    current_v = log_file.readline()
+    # version:xxx
+    current_v = current_v[7:]
+    if v<current_v:
+        print('已是最新版本')
+    else:
+        url_new_version = ufile+v
+        # download zip
+        print('downloading')
+        url = url_new_version 
+        r = requests.get(url) 
+        with open("./update_tmp/tmp.zip", "wb") as code:
+            code.write(r.content)
+        # extract zip
+        z = zipfile.ZipFile('./update_tmp/tmp.zip', 'r')
+        z.extractall(path='./update_tmp')
+        z.close()
+        print('升级完成')
