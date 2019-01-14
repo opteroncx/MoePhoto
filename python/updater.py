b3f7c51441ac46523009dde611d7e49acfe706ff
opteroncx <opteroncx@gmail.com>
2019-01-14 14:40:27 +0800

    Update updater.py

--- python/updater.py
+++ python/updater.py
@@ -4,10 +4,21 @@ import os
 import zipfile
 import requests
 import codecs
+import shutil
 
 releases = 'http://www.may-workshop.com/moephoto/version.html'
 ufile = 'http://www.may-workshop.com/moephoto/files/'
 
+
+def copyfile(srcfile,dstfile):
+    if not os.path.isfile(srcfile):
+        print("%s not exist!"%(srcfile))
+    else:
+        fpath,fname=os.path.split(dstfile)    #分离文件名和路径
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)                #创建路径
+        shutil.copyfile(srcfile,dstfile)      #复制文件
+        print("copy %s -> %s"%( srcfile,dstfile)) 
 
 def getVersion(releases=releases):
     f = requests.get(releases)
@@ -38,9 +49,15 @@ def update():
         z = zipfile.ZipFile('./update_tmp/tmp.zip', 'r')
         z.extractall(path='./update_tmp')
         z.close()
-        print('升级完成')
         # copy files
-
+        print('copying files')
+        py_files = os.listdir('./update_tmp')
+        for f in py_files:
+            if f[-3:] == '.py':
+                copyfile('./update_tmp/'+f,'./python/update_tmp/'+f)
+        print('升级完成')
+        
+        
 
 if __name__ == '__main__':
     v = getVersion()
