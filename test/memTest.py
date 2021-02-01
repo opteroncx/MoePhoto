22393908935237e427dced40153532565b16c514
lotress <cmyan6@gmail.com>
2021-02-01 15:20:51 +0800

    update memTest.py

--- test/memTest.py
+++ test/memTest.py
@@ -4,17 +4,17 @@ import torch
 from procedure import genProcess
 from config import config, process
 
-getMemUsed = torch.cuda.max_memory_cached if config.cuda else lambda: process.memory_info()[0]
-step = dict(op='dehaze', model='sun')
+getMemUsed = (lambda i: torch.cuda.memory_stats(i)['reserved_bytes.all.peak']) if config.cuda else lambda *_: process.memory_info()[0]
+step = dict(op='dehaze', model='moire_obj')
 load = 3 << 20
 p, _ = genProcess([step], True, dict(bitDepth=8, channel=0, source=0, load=load))
-t = torch.randn((3, 1024, 1024), dtype=config.dtype(), device=config.device()) # pylint: disable=E1101
-m = getMemUsed()
+t = torch.randn((3, 1440, 900), dtype=config.dtype(), device=config.device()) # pylint: disable=E1101
+m = getMemUsed(config.device())
 print(config.dtype(), m)
 sys.stdin.readline()
 if config.cuda:
-  torch.cuda.reset_max_memory_cached()
+  getMemUsed(config.device())
 p(t)
-m = getMemUsed() if config.cuda else (getMemUsed() - m)
+m = getMemUsed(config.device()) if config.cuda else (getMemUsed() - m)
 print(m, m / load)
 sys.stdin.readline()
